dacbfedf5bac54855db40211dfddfae662b8f75d
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-11-19 16:29:36 +0800

    a4 overflow

--- src/lib/components/a4.tsx
+++ src/lib/components/a4.tsx
@@ -1,3 +1,14 @@
+import { classNames } from "lib/utils";
+
 export const A4: React.FCC = ({ children }) => {
-  return <div className="h-[29.7cm] w-[21cm] bg-white shadow">{children}</div>;
+  return (
+    <div
+      className={classNames(
+        "h-[29.7cm] w-[21cm] bg-white",
+        "last:h-[29.699cm]"
+      )}
+    >
+      {children}
+    </div>
+  );
 };
