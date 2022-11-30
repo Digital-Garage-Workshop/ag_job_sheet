af9032fc5e647f2eb74b4579252185577133a81f
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-11-30 15:06:53 +0800

    set html2canvas.scale to 4

--- src/lib/utils/generate_pdf.js
+++ src/lib/utils/generate_pdf.js
@@ -1,9 +1,10 @@
 const options = {
-  html2canvas: { scrollX: 0, scrollY: 0 },
+  html2canvas: {
+    scale: 4,
+    scrollX: 0,
+    scrollY: 0,
+  },
 };
 export function generatePDF(element, filename = new Date().toISOString()) {
-  html2pdf()
-    .set({ ...options, ...{ filename: `${filename}.pdf` } })
-    .from(element)
-    .save();
+  html2pdf().set(options).from(element).save(`${filename}.pdf`);
 }
