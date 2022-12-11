fbb5555d7a8134faca9a3737d6fa74561f126149
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-12-11 19:29:37 +0800

    playwright to puppeteer

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -1,6 +1,7 @@
 import edgeChromium from "chrome-aws-lambda";
 import type { NextApiRequest, NextApiResponse } from "next";
-import playwright from "playwright";
+// import playwright from "playwright";
+import puppeteer from "puppeteer-core";
 
 // https://gist.github.com/kettanaito/56861aff96e6debc575d522dd03e5725
 const LOCAL_CHROME_EXECUTABLE =
@@ -11,7 +12,7 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   switch (method) {
     case "POST":
       const { pageRanges, path } = JSON.parse(request.body);
-      const pdf = await generatePDF2({
+      const pdf = await generatePDF({
         pageRanges,
         path: new URL(path, request.headers.origin).toString(),
       });
@@ -29,21 +30,28 @@ interface IOptions {
   pageRanges?: string;
   path: string;
 }
-export const generatePDF2 = async ({ pageRanges, path }: IOptions) => {
+export const generatePDF = async ({ pageRanges, path }: IOptions) => {
   const executablePath =
     (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
-  const browser = await playwright.chromium.launch({ executablePath });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto(path, { waitUntil: "networkidle" });
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+  });
+  const page = await browser.newPage();
+  await page.goto(path, { waitUntil: "networkidle0" });
 
-  const pdfGenerator = page.locator(".fixed");
-  await pdfGenerator.evaluate(
-    (element) => (element.style.visibility = "hidden")
+  await page.$eval(
+    ".fixed",
+    (node) => ((node as any).style.visibility = "hidden")
+  );
+
+  const pdfGenerator = await page.$(".fixed");
+  await pdfGenerator?.evaluate(
+    (node) => ((node as any).style.visibility = "hidden")
   );
 
   const pdf = await page.pdf({
-    format: "A4",
+    format: "a4",
     pageRanges,
     preferCSSPageSize: true,
     printBackground: true,
@@ -51,4 +59,23 @@ export const generatePDF2 = async ({ pageRanges, path }: IOptions) => {
   await browser.close();
 
   return pdf;
+  // const browser = await playwright.chromium.launch();
+  // const context = await browser.newContext();
+  // const page = await context.newPage();
+  // await page.goto(path, { waitUntil: "networkidle" });
+
+  // const pdfGenerator = page.locator(".fixed");
+  // await pdfGenerator.evaluate(
+  //   (node) => (node.style.visibility = "hidden")
+  // );
+
+  // const pdf = await page.pdf({
+  //   format: "A4",
+  //   pageRanges,
+  //   preferCSSPageSize: true,
+  //   printBackground: true,
+  // });
+  // await browser.close();
+
+  // return pdf;
 };
