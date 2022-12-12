e8b1d540f7b51b3b15db50a2daff932d74212425
zolboo-e <90889294+zolboo-e@users.noreply.github.com>
2022-12-12 14:41:13 +0800

    loader

--- src/lib/components/pdf_generator.tsx
+++ src/lib/components/pdf_generator.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 //
-import { useCallback } from "react";
+import { DocumentChartBarIcon } from "@heroicons/react/24/solid";
+import { useCallback, useState } from "react";
+//
+import { classNames } from "lib/utils";
 
 interface IPDFGenerator {
   filename?: string;
@@ -13,8 +16,10 @@ export const PDFGenerator: React.FC<IPDFGenerator> = ({
   pageRanges,
   path,
 }) => {
+  const [isGenerating, setIsGenerating] = useState(false);
   const _generatePDF = useCallback(async () => {
     try {
+      setIsGenerating(true);
       const response = await fetch(
         "https://pdf-generator-4led.onrender.com/pdf",
         {
@@ -33,14 +38,28 @@ export const PDFGenerator: React.FC<IPDFGenerator> = ({
       a.remove();
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsGenerating(false);
     }
   }, [filename, pageRanges, path]);
 
   return (
-    <div className="fixed left-0 right-0 bottom-0 bg-gray-400 px-10 py-2.5">
-      <button className="btn" onClick={_generatePDF}>
-        Хэвлэх
-      </button>
-    </div>
+    <>
+      <div className="fixed left-0 right-0 bottom-0 bg-gray-400 px-10 py-2.5">
+        <button className="btn" onClick={_generatePDF}>
+          Хэвлэх
+        </button>
+      </div>
+      <div
+        className={classNames(
+          "fixed inset-0 flex h-screen w-screen items-center justify-center bg-black/50 duration-500",
+          !isGenerating && "invisible opacity-0"
+        )}
+      >
+        <div className="animate-bounce">
+          <DocumentChartBarIcon className="h-10 w-10 text-white" />
+        </div>
+      </div>
+    </>
   );
 };
